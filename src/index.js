09c0c9a5d3faa75b69114a3f2198a9577568224d
Jason Sutter <jasonsutter87@gmail.com>
2017-10-16 12:13:29 -0700

    Local Storage persistence (#3)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import App from './components/dashboard';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import BirdieOpReducer from './reducers';
+import { loadState, saveState} from './localStorage'
+const persistedState = loadState();
 
 const store = createStore(
   BirdieOpReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
